274acf98fa7845f6b6afdfb8af6fc0e37ac54c3e
lprice93 <lprice@wilbron.com>
2019-05-12 15:04:42 -0500

    complete DOM DOM DOMM

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ btn.append(btnText);
 document.body.append(btn);
 btn.style.display = 'block';
 
-// let container = 0;
+let container = document.createElement('container');
+document.body.append(container);
 let counter = 0;
 
+// add new div to page when button is clicked
 btn.addEventListener('click', function () {
     let myDiv = document.createElement('div');
-
     document.body.append(myDiv);
+    container.append(myDiv);
     myDiv.className = 'myDiv';
     myDiv.style.cssFloat = 'left';
     myDiv.style.lineHeight = '100px';
     myDiv.style.margin = '5px';
+
+    // value of square id should display in center when hovered over
     myDiv.id = counter;
     let hoverText = document.createTextNode(counter)
     counter++;
@@ -24,40 +28,36 @@ btn.addEventListener('click', function () {
         myDiv.style.color = 'peachpuff';
     });
 
+    // id should disappear when mouse is no longer hovering over square
     myDiv.addEventListener('mouseout', function () {
         hoverText.remove(); // keep what you want to remove on the left bc .remove removes anything to the left of the dot. has to be an element or text node.
-        // myDiv.style.color = 'black';
     });
 
+    // square should change to random color when clicked
     myDiv.addEventListener('click', function () {
         function getRandomColor() {
             let colors = ['red', 'lightblue', 'lightgreen', 'salmon', 'olive', 'darkcyan', 'pink', 'plum'];
             let randomIndex = Math.floor(Math.random() * colors.length);
             return colors[randomIndex];
-        }
-        myDiv.style.backgroundColor = getRandomColor();
-        // console.log(getRandomColor());
+        } myDiv.style.backgroundColor = getRandomColor();
     });
 
+    // when double clicked, even square should remove square after it and odd squares should remove squares before it. if none; alert
     myDiv.addEventListener('dblclick', function () {
-        // console.log(myDiv.id % 2 === 0);
         if (myDiv.id % 2 === 0) {
             if (myDiv.nextSibling === null) {
-               alert('Nothing is there!');
-                // if there isn't a sibling, alert there is none
+                alert('Nothing is there!');
             } else {
-                // if there is a sibling, remove it.
                 myDiv.nextSibling.remove();
             };
-            // console.log(myDiv.nextSibling);
         } else {
             if (myDiv.previousSibling === null) {
                 alert('Nothing is there!');
-             } else {
-                 myDiv.previousSibling.remove();
-             };
+            } else {
+                myDiv.previousSibling.remove({
+                    container
+                });
+            };
         };
     });
 });
-
-// remember to check developer tools > elements tab to make sure it is working.
